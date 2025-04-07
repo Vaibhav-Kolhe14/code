eaa69fa7795f05b58e40a8a6c886da4fc701d459
Vaibhav14 <135616354+Vaibhav-Kolhe14@users.noreply.github.com>
2025-04-08 01:36:52 +0530

    Delete WT directory

--- WT/Pract7/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$id = $_POST['id'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-$conn->query("UPDATE students SET name='$name', email='$email' WHERE id=$id");
-header("Location: index.php");
-?>
